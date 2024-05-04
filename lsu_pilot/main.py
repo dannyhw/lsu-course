0febf1bfa517c8ea44d490902ec58a6cd93c92cf
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-05-05 00:30:39 +0100

    re-run embedding with script directly copied

--- lsu_pilot/main.py
+++ lsu_pilot/main.py
@@ -1,23 +1,24 @@
+from questions import answer_question
 import os
 import logging
-import pandas
-import numpy
-import pathlib
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-from dotenv import load_dotenv
 from openai import OpenAI
-from questions import answer_question
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv
+import pathlib
 
-load_dotenv()  # take environment variables from .env.
+load_dotenv()
 
-
-openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-tg_bot_token = os.getenv("TG_BOT_TOKEN")
+openai = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+tg_bot_token = os.environ['TG_BOT_TOKEN']
 
 project_root = pathlib.Path(__file__).parent.resolve()
 embeddings_file_path = project_root / 'processed' / 'embeddings.csv'
 
+df = pd.read_csv(embeddings_file_path, index_col=0)
+df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
 
 messages = [{
     "role": "system",
@@ -28,18 +29,11 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
 
-source_data_frame = pandas.read_csv(
-    embeddings_file_path, index_col=0)
-
-source_data_frame['embeddings'] = source_data_frame['embeddings'].apply(
-    eval).apply(numpy.array)
-
 
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     messages.append({"role": "user", "content": update.message.text})
     completion = openai.chat.completions.create(model="gpt-3.5-turbo",
                                                 messages=messages)
-
     completion_answer = completion.choices[0].message
     messages.append(completion_answer)
 
@@ -48,9 +42,7 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def mozilla(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    answer = answer_question(
-        source_data_frame, question=update.message.text, debug=True)
-
+    answer = answer_question(df, question=update.message.text, debug=True)
     await context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
 
 
@@ -58,19 +50,17 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id,
                                    text="I'm a bot, please talk to me!")
 
-if __name__ == "__main__":
-    # Set up the Telegram bot with the provided token.
+
+if __name__ == '__main__':
     application = ApplicationBuilder().token(tg_bot_token).build()
 
-    # Define command handlers for starting the bot and chatting.
-    start_handler = CommandHandler("start", start)
-    chat_handler = CommandHandler("chat", chat)
-    mozilla_handler = CommandHandler('mozilla', mozilla)
+    start_handler = CommandHandler('start', start)
+    chat_handler = CommandHandler('chat', chat)
 
-    # Add command handlers to the application.
-    application.add_handler(start_handler)
-    application.add_handler(chat_handler)
+    mozilla_handler = CommandHandler('mozilla', mozilla)
     application.add_handler(mozilla_handler)
 
-    # Start the bot and poll for new messages.
+    application.add_handler(start_handler)
+    application.add_handler(chat_handler)
+
     application.run_polling()
--- lsu_pilot/questions.py
+++ lsu_pilot/questions.py
@@ -1,22 +1,19 @@
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
 from openai import OpenAI
+import os
 from typing import List
 from scipy import spatial
 from dotenv import load_dotenv
 
-import os
-
 load_dotenv()
-
-openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
 def distances_from_embeddings(
     query_embedding: List[float],
     embeddings: List[List[float]],
     distance_metric="cosine",
-) -> List[List]:
+) -> List[float]:
     """Return the distances between a query embedding and a list of embeddings."""
     distance_metrics = {
         "cosine": spatial.distance.cosine,
@@ -24,38 +21,42 @@ def distances_from_embeddings(
         "L2": spatial.distance.euclidean,
         "Linf": spatial.distance.chebyshev,
     }
-
     distances = [
         distance_metrics[distance_metric](query_embedding, embedding)
         for embedding in embeddings
     ]
-
     return distances
 
 
-def create_context(question: str, source_data_frame: pandas.DataFrame, max_length=1800):
+openai = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+
+# df = pd.read_csv('processed/embeddings.csv', index_col=0)
+# df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
+
+
+def create_context(question, df, max_len=1800):
     """
       Create a context for a question by finding the most similar context from the dataframe
-    """
+      """
     # Get the embeddings for the question
-    question_embeddings = openai.embeddings.create(
+    q_embeddings = openai.embeddings.create(
         input=question, model='text-embedding-ada-002').data[0].embedding
 
     # Get the distances from the embeddings
-    source_data_frame['distances'] = distances_from_embeddings(question_embeddings,
-                                                               source_data_frame['embeddings'].values,
-                                                               distance_metric='cosine')
+    df['distances'] = distances_from_embeddings(q_embeddings,
+                                                df['embeddings'].values,
+                                                distance_metric='cosine')
 
     returns = []
-    current_length = 0
+    cur_len = 0
 
     # Sort by distance and add the text to the context until the context is too long
-    for i, row in source_data_frame.sort_values('distances', ascending=True).iterrows():
+    for i, row in df.sort_values('distances', ascending=True).iterrows():
         # Add the length of the text to the current length
-        current_length += row['n_tokens'] + 4
+        cur_len += row['n_tokens'] + 4
 
         # If the context is too long, break
-        if current_length > max_length:
+        if cur_len > max_len:
             break
 
         # Else add it to the text that is being returned
@@ -65,30 +66,21 @@ def create_context(question: str, source_data_frame: pandas.DataFrame, max_lengt
     return "\n\n###\n\n".join(returns)
 
 
-def answer_question(source_data_frame: pandas.DataFrame,
+def answer_question(df,
                     model="gpt-3.5-turbo-1106",
                     question="What is the meaning of life?",
-                    max_length=1800,
+                    max_len=1800,
                     debug=False,
                     max_tokens=150,
                     stop_sequence=None):
-
-    print("Answering question: " + question)
-    print("Model: " + model)
-    print("Max length: " + str(max_length))
-    print("Debug: " + str(debug))
-    print("Max tokens: " + str(max_tokens))
-    print("Stop sequence: " + str(stop_sequence))
-
     """
       Answer a question based on the most similar context from the dataframe texts
-    """
+      """
     context = create_context(
         question,
-        source_data_frame,
-        max_length,
+        df,
+        max_len=max_len,
     )
-
     # If debug, print the raw model response
     if debug:
         print("Context:\n" + context)
@@ -102,7 +94,7 @@ def answer_question(source_data_frame: pandas.DataFrame,
                 "role":
                 "user",
                 "content":
-                f"Answer the question based on the context below, and if the question can't be answered based on the context, say \"I don't know.\" Try to site sources to the links in the context when possible.\n\nContext: {context}\n\n---\n\nQuestion: {question}\nSource:\nAnswer:",
+                f"Answer the question based on the context below, and if the question can't be answered based on the context, say \"I don't know.\" Try to site sources to the links in the context when possible.\n\nContext: {context}\n\n---\n\nQuestion: {question}\nAnswer:",
             }],
             temperature=0,
             max_tokens=max_tokens,
@@ -111,14 +103,7 @@ def answer_question(source_data_frame: pandas.DataFrame,
             presence_penalty=0,
             stop=stop_sequence,
         )
-
         return response.choices[0].message.content
-
     except Exception as e:
         print(e)
         return ""
-
-
-# data_frame = pandas.read_csv('processed/embeddings.csv', index_col=0)
-# data_frame['embeddings'] = data_frame['embeddings'].apply(
-#     eval).apply(numpy.array)
