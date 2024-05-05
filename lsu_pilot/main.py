d2ccd4f379ac3544dd729da90d2fc686277ecb7b
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-05-05 17:26:10 +0100

    function calling and code generation

--- lsu_pilot/main.py
+++ lsu_pilot/main.py
@@ -1,13 +1,21 @@
+import json
 import os
 import logging
 import pandas
 import numpy
 import pathlib
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+    MessageHandler,
+    filters
+)
 from dotenv import load_dotenv
 from openai import OpenAI
 from questions import answer_question
+from functions import functions, run_function
 
 load_dotenv()  # take environment variables from .env.
 
@@ -18,10 +26,46 @@ tg_bot_token = os.getenv("TG_BOT_TOKEN")
 project_root = pathlib.Path(__file__).parent.resolve()
 embeddings_file_path = project_root / 'processed' / 'embeddings.csv'
 
+CODE_PROMPT = """
+Here are two input:output examples for code generation. Please use these and follow the styling for future requests that you think are pertinent to the request.
+Make sure All HTML is generated with the JSX flavoring.
+// SAMPLE 1
+// A Blue Box with 3 yellow cirles inside of it that have a red outline
+<div style={{   backgroundColor: 'blue',
+  padding: '20px',
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  width: '300px',
+  height: '100px', }}>
+  <div style={{     backgroundColor: 'yellow',
+    borderRadius: '50%',
+    width: '50px',
+    height: '50px',
+    border: '2px solid red'
+  }}></div>
+  <div style={{     backgroundColor: 'yellow',
+    borderRadius: '50%',
+    width: '50px',
+    height: '50px',
+    border: '2px solid red'
+  }}></div>
+  <div style={{     backgroundColor: 'yellow',
+    borderRadius: '50%',
+    width: '50px',
+    height: '50px',
+    border: '2px solid red'
+  }}></div>
+</div>
+"""
+
 
 messages = [{
     "role": "system",
     "content": "You are a helpful assistant that answers questions."
+}, {
+    "role": "system",
+    "content": CODE_PROMPT
 }]
 
 logging.basicConfig(
@@ -37,14 +81,64 @@ source_data_frame['embeddings'] = source_data_frame['embeddings'].apply(
 
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     messages.append({"role": "user", "content": update.message.text})
-    completion = openai.chat.completions.create(model="gpt-3.5-turbo",
-                                                messages=messages)
 
-    completion_answer = completion.choices[0].message
-    messages.append(completion_answer)
+    initial_response = openai.chat.completions.create(model="gpt-3.5-turbo",
+                                                      messages=messages,
+                                                      tools=functions)
 
-    await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   text=completion_answer.content)
+    initial_response_message = initial_response.choices[0].message
+
+    messages.append(initial_response_message)
+
+    final_response = None
+
+    tool_calls = initial_response_message.tool_calls
+
+    if tool_calls:
+        for tool_call in tool_calls:
+            name = tool_call.function.name
+
+            args = json.loads(tool_call.function.arguments)
+
+            response = run_function(name, args)
+
+            print(tool_calls)
+
+            messages.append({
+                "tool_call_id": tool_call.id,
+                "role": "tool",
+                "name": name,
+                "content": str(response),
+            })
+
+            if name == 'svg_to_png_bytes':
+                await context.bot.send_photo(chat_id=update.effective_chat.id,
+                                             photo=response)
+
+            # Generate the final response
+            final_response = openai.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=messages,
+            )
+
+            final_answer = final_response.choices[0].message
+
+            # Send the final response if it exists
+            if (final_answer):
+                messages.append(final_answer)
+
+                await context.bot.send_message(chat_id=update.effective_chat.id,
+                                               text=final_answer.content)
+            else:
+                # Send an error message if something went wrong
+                await context.bot.send_message(
+                    chat_id=update.effective_chat.id,
+                    text='something went wrong, please try again')
+
+    # no functions were executed
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text=initial_response_message.content)
 
 
 async def mozilla(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -64,7 +158,7 @@ if __name__ == "__main__":
 
     # Define command handlers for starting the bot and chatting.
     start_handler = CommandHandler("start", start)
-    chat_handler = CommandHandler("chat", chat)
+    chat_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), chat)
     mozilla_handler = CommandHandler('mozilla', mozilla)
 
     # Add command handlers to the application.
